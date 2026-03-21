3646a356b7b4aa3e79823d093241a3684a176c8b
ramosdetrigo <julia.and.ramos@gmail.com>
2026-03-20 22:11:54 -0300

    miner initial commit

---
+++ miner.lua
@@ -0,0 +1,45 @@
+local miner = require "walker"
+local utils = require "utils"
+
+local vec3 = utils.vec3
+
+function miner:mineVein(start)
+    local stack = {} -- block stack
+    local found = {} -- found blocks hashmap
+
+    stack[#stack + 1] = start
+    found[start:hash()] = true
+
+    -- Depth-first algorithm
+    while #stack ~= 0 do
+        local block = stack[#stack]
+        self:mineTo(block) -- visits block
+
+        -- finds non-visited neighbour
+        local neighbour
+        for n in self:neighbourOres() do
+            if found[n:hash()] then
+                neighbour = n
+                break
+            end
+        end
+
+        if neighbour then
+            -- if found, adds neighbour to the top of the stack
+            stack[#stack + 1] = neighbour
+            found[neighbour:hash()] = true
+        else
+            -- else, pops the stack
+            table.remove(stack, #stack)
+        end
+    end
+end
+
+
+function miner:mineTo(target)
+    -- TODO: move to coordinate while mining everything in front
+end
+
+function miner:neighbouringOres()
+    -- TODO: return coords of ores around the turtle
+end
--- utils.lua
+++ utils.lua
@@ -1,4 +1,6 @@
-local utils = {}
+local utils = {
+}
+
 function utils.printvec(v, msg)
     msg = msg.." " or ""
     print(msg.."(x: "..v.x.." y: "..v.y..")")
@@ -7,5 +9,54 @@ end
 function utils.sign(x)
     return x > 0 and 1 or (x == 0 and 0 or -1)
 end
+
+local vec3 = {}
+
+vec3.__index = vec3
+
+function vec3:__call(x, y, z)
+    return setmetatable({ x = x, y = y, z = z }, self)
+end
+
+function vec3:hash()
+    return self.x .. "|" .. self.y .. "|" .. self.z
+end
+
+function vec3:add(rhs)
+    return vec3(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+end
+
+function vec3:sub(rhs)
+    return vec3(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+end
+
+function vec3:mul(scalar)
+    return vec3(self.x * scalar, self.y * scalar, self.z * scalar)
+end
+
+function vec3:div(scalar)
+    return vec3(self.x / scalar, self.y / scalar, self.z / scalar)
+end
+
+function vec3:rotate90(n)
+    local fn = (n > 0) and self.rotate90Right or self.rotate90Left
+    local v = self
+    for _ = 1, math.abs(n) do
+        v = fn(v)
+    end
+    return v
+end
+
+function vec3:rotate90Left()
+    return vec3(self.z, self.y, -self.x)
+end
+
+function vec3:rotate90Right()
+    return vec3(-self.z, self.y, self.x)
+end
+
+setmetatable(vec3, vec3)
+
+utils.vec3 = vec3
 
 return utils
